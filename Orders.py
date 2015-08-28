28dd744d9e6d29d6785292a650ceda56a7911d64
AlgoVentures <AlgoVentures@users.noreply.github.com>
2015-08-28 16:00:41 -0700

    Added Docstrings

--- Orders.py
+++ Orders.py
@@ -1,80 +1,59 @@
+
 import json
 import requests
 from ConfigParser import SafeConfigParser
 
+
 #initialize parser to get secret data, end points, etc.
-#put you API Key and Endpoints in file called config.ini
 parser = SafeConfigParser()
 parser.read('config.ini')
 
-def Buy_Stock_Limit(target, quantity, limit_price, duration):
+def Get_Quote(ticker):
+    """returns full order quote for given ticker, function used internally in functions like 'Get_Open' and 'Get_Ask'"""
     #set query args
-    endpoint = parser.get('endpoint', 'brokerage') + 'accounts/' + parser.get('account', 'AccountNumber') + '/orders'
+    endpoint = parser.get('endpoint', 'brokerage') + 'markets/quotes'
     headers = {'Authorization': parser.get('account', 'Auth'), 'Accept': parser.get('message_format', 'accept_format')}
-    payload = {'class' : 'equity',
-                'symbol' : target,
-                'duration' : duration,
-                'side' : 'buy',
-                'quantity' : quantity,
-                'type' : 'limit',
-                'price' : limit_price,
-                'preview' : parser.get('message_format', 'preview')}
-    
-    #send query
-    r = requests.post(endpoint, headers = headers , params = payload)
-    
-    #get parse response
-    content = json.loads(r.text)
-    return content
-
-def Sell_Stock_Limit(target, quantity, limit_price, duration):
-    #set query args
-    endpoint = parser.get('endpoint', 'brokerage') + 'accounts/' + parser.get('account', 'AccountNumber') + '/orders'
-    headers = {'Authorization': parser.get('account', 'Auth'), 'Accept': parser.get('message_format', 'accept_format')}
-    payload = {'class' : 'equity',
-                'symbol' : target,
-                'duration' : duration,
-                'side' : 'sell',
-                'quantity' : quantity,
-                'type' : 'limit',
-                'price' : limit_price,
-                'preview' : parser.get('message_format', 'preview')}
-    
-    #send query
-    r = requests.post(endpoint, headers = headers , params = payload)
-    
-    #get parse response
-    content = json.loads(r.text)
-    return content
-
-    
-def Cancel_Order(Order_Number):
-    endpoint = parser.get('endpoint', 'brokerage') + 'accounts/' + parser.get('account', 'AccountNumber') + '/orders' + Order_Number
-    headers = {'Authorization': parser.get('account', 'Auth'), 'Accept': parser.get('message_format', 'accept_format')}
-    
-    #send query
-    r = requests.delete(endpoint, headers = headers)
-    #get parse response
-    content = json.loads(r.text)
-    
-    if (content['order']['id'] == Order_Number and 
-        content['order']['status'] == 'ok'):
-        print 'Order ' + Order_Number + ' Canceled'
-    else:
-        print 'Order Failed to Cancel.  Status: ' + content['order']['status']
-
-
-def Check_Order_Status(order_id):
-    #set query args
-    endpoint = parser.get('endpoint', 'brokerage') + 'accounts/' + parser.get('account', 'AccountNumber') + '/orders/' + order_id
-    headers = {'Authorization': parser.get('account', 'Auth'), 'Accept': parser.get('message_format', 'accept_format')}
-    payload = {'account_id' :  parser.get('account', 'AccountNumber'), 'id' : order_id}
+    payload = {'symbols' : ticker}
     
     #send query
     r = requests.get(endpoint, headers = headers , params = payload)
-    
+    #print r.text
     #get parse response
     content = json.loads(r.text)
     return content
 
-                
+def Get_Market_Status():
+    """Checks if market is open or closed, 1 if open, 0 if not open"""
+    endpoint = parser.get('endpoint', 'brokerage') + 'markets/clock'
+    headers = {'Authorization': parser.get('account', 'Auth'), 'Accept': parser.get('message_format', 'accept_format')}
+    
+     #send query
+    r = requests.get(endpoint, headers = headers)
+    #print r.text
+    #get parse response
+    content = json.loads(r.text)
+    
+    if content['clock']['state'] == 'open':
+        return 1
+    else:
+        return 0
+
+def Get_Open(ticker):
+    """returns opening price of target security"""
+    content = Get_Quote(ticker)
+    open_price = content['quotes']['quote']['open']
+    return open_price
+
+def Get_Ask(ticker):
+    """returns current best ask price of  target security""" 
+    content = Get_Quote(ticker)
+    ask_price = content['quotes']['quote']['ask']
+    ask_size = content['quotes']['quote']['asksize']
+    return {'ask_price':ask_price, 'ask_size':ask_size}
+
+def Get_Bid(ticker):
+    """returns current best bid priceof  target security"""
+    content = Get_Quote(ticker)
+    bid_price = content['quotes']['quote']['bid']
+    bid_size = content['quotes']['quote']['bidsize']
+    return {'bid_price':bid_price, 'bid_size':bid_size}
